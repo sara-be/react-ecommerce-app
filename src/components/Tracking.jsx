0902df44d3d4780abcd9af0bac05c6a909a63386
Sara <sarabenaddi0@gmail.com>
2026-01-30 10:33:54 +0100

    React Componenets & Routes

---
+++ src/components/Tracking.jsx
@@ -0,0 +1,78 @@
+import { Link } from "react-router";
+const Tracking = () => {
+    return (
+        <>
+    <div className="header">
+      <div className="left-section">
+        <Link to="/" className="header-link">
+          <img className="logo"
+            src="/logo-white.png" />
+          <img className="mobile-logo"
+            src="/mobile-logo-white.png" />
+        </Link>
+      </div>
+
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" />
+
+        <button className="search-button">
+          <img className="search-icon" src="/icons/search-icon.png" />
+        </button>
+      </div>
+
+      <div className="right-section">
+        <Link className="orders-link header-link" to="/orders">
+
+          <span className="orders-text">Orders</span>
+        </Link>
+
+        <Link className="cart-link header-link" to="/checkout">
+          <img className="cart-icon" src="/icons/cart-icon.png" />
+          <div className="cart-quantity">3</div>
+          <div className="cart-text">Cart</div>
+        </Link>
+      </div>
+    </div>
+
+    <div className="tracking-page">
+      <div className="order-tracking">
+        <Link className="back-to-orders-link link-primary" to="/orders">
+          View all orders
+        </Link>
+
+        <div className="delivery-date">
+          Arriving on Monday, June 13
+        </div>
+
+        <div className="product-info">
+          Black and Gray Athletic Cotton Socks - 6 Pairs
+        </div>
+
+        <div className="product-info">
+          Quantity: 1
+        </div>
+
+        <img className="product-image" src="/products/athletic-cotton-socks-6-pairs.jpg" />
+
+        <div className="progress-labels-container">
+          <div className="progress-label">
+            Preparing
+          </div>
+          <div className="progress-label current-status">
+            Shipped
+          </div>
+          <div className="progress-label">
+            Delivered
+          </div>
+        </div>
+
+        <div className="progress-bar-container">
+          <div className="progress-bar"></div>
+        </div>
+      </div>
+    </div>
+  </>
+    );
+}
+
+export default Tracking;
